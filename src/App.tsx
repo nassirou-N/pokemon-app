df7e7c718383c9e0326b1f6c0aaf09be6ff749d0
nassirou <nassirou9993@gmail.com>
2023-09-02 11:53:28 +0100

    the detail page pokemons en redirectory

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PokemonList from './pages/pokemons-list';
+import PokemonsDetail from './pages/pokemon-detail';
+import { BrowserRouter as Router,Route,  Routes,Link } from 'react-router-dom';
 
 
 
@@ -7,9 +9,24 @@ function App() {
   
 
   return (
-    <>
-      <PokemonList />
-    </>
+    <Router>
+      <div>
+
+    
+        {/**bar de navigation */}
+        <nav>
+          <div className='nav-wrapper teal'>
+            <Link to="/" className='brand-logo center'>Pokédéx</Link>
+          </div>
+        </nav>
+        {/** le systeme de gestions des routes */}
+          <Routes>
+            <Route  path='/' element={<PokemonList/>} />
+            <Route  path='/pokemons' element={<PokemonList/>} />
+            <Route  path='/pokemons/:id' element={<PokemonsDetail/>} />
+          </Routes>
+        </div>
+    </Router>
   );
 }
 
--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,9 +1,10 @@
 import React, { FunctionComponent, useState } from 'react'
-import { server } from 'typescript';
+
 import Pokemon from '../models/pokemon';
 import './pokemon-card.css';
 import formatDate from '../helpers/format-date';
 import formatType from '../helpers/format-types';
+import { Link } from 'react-router-dom';
 
 type Props ={
     pokemon:Pokemon;
@@ -25,22 +26,24 @@ const PokemonCard:FunctionComponent<Props> = ({pokemon,borderColor='#009688'}) =
 
    
     return (
-        <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
-        <div className="card horizontal" style={{borderColor: color}}>
-          <div className="card-image"> 
-            <img src={pokemon.picture} alt={pokemon.name}/>
-          </div>
-          <div className="card-stacked">
-            <div className="card-content">
-              <p>{pokemon.name}</p>
-              <p><small>{formatDate(pokemon.created)}</small></p>
-              {pokemon.types.map(type=>(
-                <span key={type} className={formatType(type)}>{type}</span>
-              ))}
+      <Link to={`/pokemons/${pokemon.id}`}>
+          <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
+          <div className="card horizontal" style={{borderColor: color}}>
+            <div className="card-image"> 
+              <img src={pokemon.picture} alt={pokemon.name}/>
             </div>
-          </div>
-        </div> 
-      </div>
+            <div className="card-stacked">
+              <div className="card-content">
+                <p>{pokemon.name}</p>
+                <p><small>{formatDate(pokemon.created)}</small></p>
+                {pokemon.types.map(type=>(
+                  <span key={type} className={formatType(type)}>{type}</span>
+                ))}
+              </div>
+            </div>
+          </div> 
+        </div>
+      </Link>
     );
 }
 
